cebffc7f2b9d11d34e854462792d70acb5fd5ddc
Gregary Pergrossi <gpergrossi@gmail.com>
2024-04-13 14:33:03 -0500

    Cleaning unit movement

--- Code/Scripts/Units/unit_base.gd
+++ Code/Scripts/Units/unit_base.gd
@@ -1,17 +1,24 @@
 class_name UnitBase extends CharacterBody3D
 
+const LAYER_WORLD := 1
+const LAYER_PLAYER := 2
+const LAYER_ENEMY := 3
+
 @export_enum("Player", "Enemy") var team := "Player" : set = set_team_str
 var team_def : Team
 
 @export var unit_name : String
-@export var top_speed := 50
-@export var move_accel := 20.0
-@export var gravity := 20.0
+@export var top_speed := 10
+@export var move_accel := 100.0
+@export var gravity := 40.0
+
+@onready var body := %Body as MeshInstance3D
 
 var _target_speed : float
 
 func _ready():
 	_target_speed = top_speed
+	set_team_str(team)
 
 
 func _physics_process(delta : float):
@@ -24,15 +31,46 @@ func _physics_process(delta : float):
 		var target_move_x := teamMoveX * _target_speed
 		velocity.x = move_toward(velocity.x, target_move_x, move_accel * delta)
 	
-	print("Updating unit. Move dir = " + str(teamMoveX) + ", Velocity = " + str(velocity))
 	move_and_slide()
 
 
 func set_team_str(val : String):
 	team = val
 	team_def = TeamDefs.FromName(team)
+	on_team_change(team_def)
 
 
 # Override if you want to pull stats from the spell definition dictionary
 func consume_spell_def(spell_def : Dictionary):
 	pass
+
+
+func on_team_change(team : Team):
+	if is_instance_valid(body):
+		var mat := body.mesh.surface_get_material(0) as StandardMaterial3D
+		if is_instance_valid(team):
+			mat.albedo_color = team.team_color
+		else:
+			mat.albedo_color = Color.GRAY
+	
+	var team_layer : int
+	var other_team_layer : int
+	match team.team_name:
+		"Player": 
+			team_layer = LAYER_PLAYER
+			other_team_layer = LAYER_ENEMY
+		"Enemy": 
+			team_layer = LAYER_ENEMY
+			other_team_layer = LAYER_PLAYER
+		_: assert(false, "Bad team!")
+	
+	# Assign to own team layer + world objects layer
+	set_collision_layer_value(LAYER_WORLD, false)
+	set_collision_layer_value(team_layer, true)
+	set_collision_layer_value(other_team_layer, false)
+	
+	# Collide with other team layer + world objects layer
+	set_collision_mask_value(LAYER_WORLD, true)
+	set_collision_mask_value(other_team_layer, true)
+	set_collision_mask_value(team_layer, false)
+		
--- Code/Scripts/Units/unit_manager.gd
+++ Code/Scripts/Units/unit_manager.gd
@@ -2,8 +2,12 @@ class_name UnitManager extends Node3D
 
 static var _unit_manager_static : UnitManager
 
+@export var summon_default_position_left : Node3D
+@export var summon_default_position_right : Node3D
+
 var scene_basic_walker := preload("res://Scenes/Units/unit_basic_walker.tscn") as PackedScene
 
+var summon_lock := false
 
 func _ready():
 	_unit_manager_static = self
@@ -12,8 +16,15 @@ func _ready():
 func _input(event : InputEvent):
 	if event is InputEventKey:
 		var key_event := event as InputEventKey
-		if key_event.physical_keycode == KEY_1:
-			summon("basicWalker", {}, Vector2(-200, 0), "Player")
+		if event.is_pressed():
+			if not summon_lock:
+				if key_event.physical_keycode == KEY_1:
+					summon("basicWalker", {}, Vector2(summon_default_position_left.global_position.x, 0), "Player")
+				if key_event.physical_keycode == KEY_2:
+					summon("basicWalker", {}, Vector2(summon_default_position_right.global_position.x, 0), "Enemy")
+			summon_lock = true
+		else:
+			summon_lock = false
 
 
 static func summonBasicWalker(spell_def : Dictionary, position : Vector2, team : String):
@@ -25,12 +36,24 @@ func summon(unitType : String, spell_def : Dictionary, position : Vector2, team 
 	var packed_scene : PackedScene = null
 	match(unitType):
 		"basicWalker": packed_scene = scene_basic_walker
-		_: 
+		_:
 			printerr("No such unit type \"" + unitType + "\"!")
 			return
 	
 	var unit := packed_scene.instantiate() as UnitBase
-	unit.team = team
+	unit.team = team	
 	unit.position = Vector3(position.x, -position.y, 0.0)
 	unit.consume_spell_def(spell_def)
 	add_child(unit)
+
+
+func _on_unit_kill_plane_left_body_entered(body : PhysicsBody3D):
+	if body is UnitBase:
+		print("Unit killed")
+		body.queue_free()
+
+
+func _on_unit_kill_plane_right_body_entered(body : PhysicsBody3D):
+	if body is UnitBase:
+		print("Unit killed")
+		body.queue_free()
