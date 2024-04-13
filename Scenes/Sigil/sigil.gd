349c0d226be2e8f6f712f389cd232d6c6fa41782
espoire <a.pergrossi@gmail.com>
2024-04-13 19:13:34 -0400

    Improve & reposition Sigil art

--- Scenes/Sigil/sigil.gd
+++ Scenes/Sigil/sigil.gd
@@ -13,34 +13,43 @@ var _current_path : Array[int] = []
 func _ready():
 	_line.clear_points()
 
-func _input(event : InputEvent) -> void:
-	var mouse_event := event as InputEventMouse
-	if not mouse_event:
-		return
-	
+func getNearestSigilVertexTo(position : Vector2):
 	var nearest_vertex_idx := -1
 	var nearest_vertex_distance := interaction_distance
 	for i in range(vertices.size()):
-		var distance = vertices[i].position.distance_to(mouse_event.position)  # could be dist^2.
+		var distance = vertices[i].get_global_position().distance_to(position)  # could be dist^2.
 		if distance < nearest_vertex_distance:
 			nearest_vertex_idx = i
 			nearest_vertex_distance = distance
+	return {
+		'index': nearest_vertex_idx,
+		'distance': nearest_vertex_distance,
+	};
+
+func _input(event : InputEvent) -> void:
+	var mouse_event := event as InputEventMouse
+	if not mouse_event: return;
+	
+	var nearest = getNearestSigilVertexTo(mouse_event.position);
 			
 	# Release sigil if we're distance from any vertex or have released the mosue.
-	if nearest_vertex_idx == -1 or mouse_event.button_mask & MOUSE_BUTTON_MASK_LEFT == 0:
+	if nearest['index'] == -1 or mouse_event.button_mask & MOUSE_BUTTON_MASK_LEFT == 0:
 		if not _current_path.is_empty():
 			_release_sigil()
 			get_viewport().set_input_as_handled()
 		return
 		
-	if nearest_vertex_distance < snap_distance:
-		_maybe_add_to_path(nearest_vertex_idx)
+	if nearest['distance'] < snap_distance:
+		_maybe_add_to_path(nearest['index'])
+
+	# Account for Sigil position and scale
+	var pos = (mouse_event.position - get_global_position()) / transform.get_scale()
 
 	if _line.get_point_count() == _current_path.size():
 		# Create initial endpoint. This is awful.
-		_line.add_point(mouse_event.position)
+		_line.add_point(pos)
 	else:
-		_line.set_point_position(_current_path.size(), mouse_event.position)
+		_line.set_point_position(_current_path.size(), pos)
 
 	get_viewport().set_input_as_handled()
 	
@@ -61,9 +70,12 @@ func _maybe_add_to_path(vertex_idx : int) -> void:
 	_current_path.push_back(vertex_idx)
 	_line.show()
 	_line.add_point(vertices[vertex_idx].position, _current_path.size() - 1)
+	vertices[vertex_idx].visible = true;
 
 func _release_sigil() -> void:
 	_line.hide()  # TODO fun animation instead.
 	rune_drawn.emit(Rune.new(_current_path))
 	_current_path.clear()
 	_line.clear_points()
+	for vert in vertices:
+		vert.visible = false;
