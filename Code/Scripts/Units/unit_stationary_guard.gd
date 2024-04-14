339381688b1f65d1380d68e474fdc5a92f13b0c4
Gregary Pergrossi <gpergrossi@gmail.com>
2024-04-14 18:27:31 -0500

    Unit fixes + Cabbage knockback tilt

--- Code/Scripts/Units/unit_stationary_guard.gd
+++ Code/Scripts/Units/unit_stationary_guard.gd
@@ -1,10 +1,13 @@
 class_name UnitStationaryGuard extends UnitBase
 
 @onready var _unit_anims := %UnitAnimations as AnimationPlayer
+@onready var _body := %BodyTransform as Node3D
 
+var _damage_taken_rolling_dps := 0.0
 
 func _ready() -> void:
 	state_changed.connect(_on_state_changed)
+	damage_taken.connect(_on_damage_taken)
 	super._ready()
 
 
@@ -13,16 +16,8 @@ func damage_target():
 	super.damage_target()
 
 
-func _on_target_acquired(_me : UnitBase, _target : UnitBase):
-	change_state(UnitState.ATTACKING)
-
-
-func _on_target_lost(_me : UnitBase, _prev_target : UnitBase):
-	change_state(UnitState.MOVING)
-
-
-func _on_target_dead(_me : UnitBase, _prev_target : UnitBase):
-	change_state(UnitState.MOVING)
+func _on_damage_taken(_me : UnitBase, damage_amount : float, new_health : float):
+	_damage_taken_rolling_dps += damage_amount
 
 
 func _on_state_changed(_me : UnitBase, new_state : UnitState, old_state : UnitState):
@@ -33,6 +28,8 @@ func _on_state_changed(_me : UnitBase, new_state : UnitState, old_state : UnitSt
 
 func process_unit(delta : float) -> void:
 	walk(0, delta)
+	_damage_taken_rolling_dps *= pow(0.01, delta * 2)
+	_body.rotation_degrees.z = (1.0 - 1.0 / (1.0 + _damage_taken_rolling_dps * 0.5)) * 30.0
 
 
 func _on_targeting_timer():
