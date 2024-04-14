f8f1587f748e116626f55a869e5ce7685d0d85d9
Gregary Pergrossi <gpergrossi@gmail.com>
2024-04-13 19:45:50 -0500

    Unit animations

--- Code/Scripts/Units/unit_base.gd
+++ Code/Scripts/Units/unit_base.gd
@@ -1,5 +1,12 @@
 class_name UnitBase extends CharacterBody3D
 
+enum UnitState {
+	MOVING,
+	ATTACKING
+}
+
+static var next_uid := 0
+
 const LAYER_WORLD := 1
 const LAYER_PLAYER := 2
 const LAYER_ENEMY := 3
@@ -8,37 +15,73 @@ const LAYER_ENEMY := 3
 var team_def : Team
 
 @export var unit_name : String
+
+@export_category("Movement")
 @export var top_speed := 10
 @export var move_accel := 100.0
 @export var gravity := 40.0
+
+@export_category("Health")
 @export var max_health := 100.0
 
-@onready var body := %Body as MeshInstance3D
-@onready var animation_player := %AnimationPlayer as AnimationPlayer
+@export_category("Attack")
+@export var attack_range := 2.0
+@export var damage := 30.0
+
+@export_category("Rendering")
+@export var damage_tint := 0.0 : set = set_damage_tint
+
+@onready var _targeting_timer := %TargetingTimer as Timer
+@onready var _ouch_animation := %OuchAnimation as AnimationPlayer
 
 var _target_speed : float
 var _health : float
 
+var _id := 0
+var _is_dead := false
+var _state := UnitState.MOVING
+var _target : UnitBase = null
+
+signal state_changed(new_state : UnitState)
+
 
 func _ready():
+	_id = next_uid
+	next_uid += 1
+	
 	_target_speed = top_speed
 	_health = max_health
 	set_team_str(team)
-	if is_instance_valid(animation_player):
-		animation_player.play("walk")
+	state_changed.connect(on_state_changed)
+	state_changed.emit(UnitState.MOVING)
+	_targeting_timer.timeout.connect(_find_target)
 
 
 func _physics_process(delta : float):
-	var teamMoveX := team_def.get_team_move_x()
-	
-	if not is_on_floor():
-		velocity += Vector3.DOWN * gravity * delta
-	
-	if absf(velocity.x - teamMoveX * _target_speed) > 0.0:
-		var target_move_x := teamMoveX * _target_speed
-		velocity.x = move_toward(velocity.x, target_move_x, move_accel * delta)
-	
-	move_and_slide()
+	if _state == UnitState.MOVING:
+		var teamMoveX := team_def.get_team_move_x()
+		
+		if not is_on_floor():
+			velocity += Vector3.DOWN * gravity * delta
+		
+		if absf(velocity.x - teamMoveX * _target_speed) > 0.0:
+			var target_move_x := teamMoveX * _target_speed
+			velocity.x = move_toward(velocity.x, target_move_x, move_accel * delta)
+		
+		move_and_slide()
+
+
+func _find_target() -> void:
+	if is_instance_valid(_target): return
+	var enemy := find_nearest_enemy(attack_range)
+	if enemy:
+		_target = enemy
+		if _state != UnitState.ATTACKING:
+			state_changed.emit(UnitState.ATTACKING)
+	else:
+		_target = null
+		if _state != UnitState.MOVING:
+			state_changed.emit(UnitState.MOVING)
 
 
 func set_team_str(val : String):
@@ -53,13 +96,6 @@ func consume_spell_def(_spell_def : Dictionary):
 
 
 func on_team_change(new_team_def : Team):
-	if is_instance_valid(body):
-		var mat := body.mesh.surface_get_material(0) as StandardMaterial3D
-		if is_instance_valid(new_team_def):
-			mat.albedo_color = new_team_def.team_color
-		else:
-			mat.albedo_color = Color.GRAY
-	
 	var team_layer : int
 	var other_team_layer : int
 	match new_team_def.team_name:
@@ -106,27 +142,78 @@ func find_nearest_enemy(max_range : float) -> UnitBase:
 	var nearest_distance_squared = max_range * max_range
 	
 	var other_units := get_tree().get_nodes_in_group(&"units")
-	print("There are " + str(len(other_units)) + " other units...")
 	
 	for in_group in other_units:  # Could speed up with 1d tree even.
 		var candidate_unit = in_group as UnitBase
 		assert(candidate_unit, "All units should be UnitBase")
 		if team_def == candidate_unit.team_def:
-			print("Same team.")
+			print("Candidate is same team")
+			continue
+		if candidate_unit._is_dead:
+			print("Candidate is dead")
 			continue
 		
-		var distance_squared = candidate_unit.global_position.distance_squared_to(candidate_unit.global_position)
+		var distance_squared = global_position.distance_squared_to(candidate_unit.global_position)
 		if distance_squared < nearest_distance_squared:
 			nearest_enemy = candidate_unit
 			nearest_distance_squared = distance_squared
-		
+		else:
+			print("Candidate is too far")
+	
+	if is_instance_valid(nearest_enemy):
+		print(to_string() + " found target " + nearest_enemy.to_string() + " at distance " + str(sqrt(nearest_distance_squared)))
 	return nearest_enemy
 
+
+func on_state_changed(new_state : UnitState):
+	var unit_anims := find_child("UnitAnimations") as AnimationPlayer
+	match(new_state):
+		UnitState.MOVING: 
+			print("Unit " + to_string() + " is now moving")
+			if is_instance_valid(unit_anims):
+				unit_anims.play("walk")
+		UnitState.ATTACKING:
+			print("Unit " + to_string() + " is now attacking")
+			if is_instance_valid(unit_anims):
+				unit_anims.play("attack")
+
+
+func _to_string():
+	return unit_name + " #" + str(_id) + " (" + team_def.team_name + ")"
+
+
+# Call this from animation sequence to actually deal damage at the right time.
+func damage_target():
+	if is_instance_valid(_target): 
+		print("Unit " + to_string() + " has damaged its target")
+		_target.take_damage(damage)
+		if _target._is_dead:
+			_target = null
+			state_changed.emit(UnitState.MOVING)
+
+
 func take_damage(damage : float) -> void:
-	print("Ouch!")
 	_health -= damage
+	_ouch_animation.play("ouch")
 	if _health < 0:
 		die()
 
+
 func die() -> void:
+	_is_dead = true
 	queue_free()
+
+
+func set_damage_tint(amount : float):
+	var render_children := find_children("*Red")
+	render_children.append_array(find_children("*Green"))
+	for child in render_children:
+		var meshInst := child as MeshInstance3D
+		if is_instance_valid(meshInst):
+			if meshInst.visible:
+				set_damage_tint_on(meshInst, amount)
+
+
+func set_damage_tint_on(meshInst : MeshInstance3D, amount : float):
+	var material := meshInst.mesh.surface_get_material(0) as ShaderMaterial
+	material.set_shader_parameter("albedo", Color.WHITE.lerp(Color.RED, amount))
--- Code/Scripts/Units/unit_basic_walker.gd
+++ Code/Scripts/Units/unit_basic_walker.gd
@@ -1,20 +1,7 @@
 class_name UnitBasicWalker extends UnitBase
 
-@export var attack_range : float
-@export var damage : float
-
-@onready var _attack_timer := %AttackTimer as Timer
-
 func _ready() -> void:
 	super._ready()
-	_attack_timer.timeout.connect(_maybe_attack)
 
-
-func _maybe_attack() -> void:
-	var enemy = find_nearest_enemy(attack_range)
-	if not enemy:
-		print("No enemy!")
-		return
-	
-	print("No enemy!")
-	enemy.take_damage(damage)
+func damage_target():
+	super.damage_target()
