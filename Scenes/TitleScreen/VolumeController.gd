91fc5500cd06ab93cacd4ed787565e9b62ebc512
espoire <a.pergrossi@gmail.com>
2024-04-14 15:10:40 -0400

    Add volume controls

---
+++ Scenes/TitleScreen/VolumeController.gd
@@ -0,0 +1,36 @@
+class_name VolumeController extends AudioStreamPlayer
+
+static var volume: float = 0.15;
+const minDb = -15;
+const maxDb = +20;
+static var db = interpolate (volume, minDb, maxDb);
+
+@onready var label = %"Volume Label";
+@onready var originalLabelText = label.text;
+
+func _ready():
+	%"Volume Slider".value_changed.connect(_on_value_changed);
+
+func _on_value_changed (value):
+	setVolume(value / 100.0);
+	var busIndex = AudioServer.get_bus_index(bus);
+	
+	if volume > 0:
+		AudioServer.set_bus_mute(busIndex, false);
+		AudioServer.set_bus_volume_db(busIndex, db);
+		if !is_playing():
+			play();
+		label.text = originalLabelText;
+	else:
+		AudioServer.set_bus_mute(busIndex, true);
+		if is_playing():
+			stop();
+		label.text = originalLabelText + " (Muted)";
+
+static func setVolume (newVolume: float):
+	volume = newVolume;
+	db = interpolate (volume, minDb, maxDb);
+
+static func interpolate (fraction: float, min: float, max: float):
+	var delta = max - min;
+	return min + delta * fraction;
