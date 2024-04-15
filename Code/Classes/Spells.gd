dd500d2ec30a8dc01dfd2cf5318e3bbbbeeb1d28
espoire <a.pergrossi@gmail.com>
2024-04-15 19:22:12 -0400

    Rebalance spells

--- Code/Classes/Spells.gd
+++ Code/Classes/Spells.gd
@@ -4,7 +4,7 @@ static var definitions = [{
 	'programmaticName': 'summonBasicWalker',
 	'name': 'Summon Broccoli',
 	'level': 1,
-	'manaSecondsAtLevel': 5,
+	'manaSecondsAtLevel': 3,
 	'preferredLocation': 'none',
 	'rune': Rune.new([0, 3, 2]), # For visual runes, see the Miro board at https://miro.com/app/board/uXjVKUq1fIo=/
 	'castFunc': UnitManager.summonBasicWalker,
@@ -13,65 +13,65 @@ static var definitions = [{
 	'programmaticName': 'summonFlyingBomber',
 	'name': 'Summon Crow',
 	'level': 2,
-	'manaSecondsAtLevel': 30,
+	'manaSecondsAtLevel': 15,
 	'preferredLocation': 'sky',
 	'rune': Rune.new([4, 1 ,2]),
 	'castFunc': UnitManager.summonFlyingBomber,
 	'botCooldownSeconds': 5,
-}, {
-	'programmaticName': 'summonRanged',
-	'name': 'Summon Tomato',
-	'level': 2,
-	'manaSecondsAtLevel': 20,
-	'preferredLocation': 'ground',
-	'rune': Rune.new([0, 3, 2, 1]),
-	'castFunc': Spells.dummy, # TODO
-	'botCooldownSeconds': 5,
+# }, {
+# 	'programmaticName': 'summonRanged',
+# 	'name': 'Summon Tomato',
+# 	'level': 2,
+# 	'manaSecondsAtLevel': 20,
+# 	'preferredLocation': 'ground',
+# 	'rune': Rune.new([0, 3, 2, 1]),
+# 	'castFunc': Spells.dummy, # TODO
+# 	'botCooldownSeconds': 5,
 }, {
 	'programmaticName': 'summonWall',
 	'name': 'Summon Lettuce',
 	'level': 3,
-	'manaSecondsAtLevel': 15,
+	'manaSecondsAtLevel': 6,
 	'preferredLocation': 'ground',
 	'rune': Rune.new([2, 0, 4, 1, 3]),
 	'castFunc': UnitManager.summonStationaryGuard,
 	'botCooldownSeconds': 5,
-}, {
-	'programmaticName': 'summonBoulder',
-	'name': 'Summon Pumpkin',
-	'level': 3,
-	'manaSecondsAtLevel': 25,
-	'preferredLocation': 'ground',
-	'rune': Rune.new([0, 1, 3, 4, 2, 0]),
-	'castFunc': Spells.dummy, # TODO
-	'botCooldownSeconds': 10,
-}, {
-	'programmaticName': 'summonFallingSpear',
-	'name': 'Summon Carrot',
-	'level': 3,
-	'manaSecondsAtLevel': 10,
-	'preferredLocation': '*',
-	'rune': Rune.new([0, 1, 4, 0]),
-	'castFunc': Spells.dummy, # TODO
-	'botCooldownSeconds': 5,
-}, {
-	'programmaticName': 'summonExploder',
-	'name': 'Summon Popcorn',
-	'level': 4,
-	'manaSecondsAtLevel': 15,
-	'preferredLocation': 'ground',
-	'rune': Rune.new([4, 1, 2]),
-	'castFunc': Spells.dummy, # TODO
-	'botCooldownSeconds': 15,
-}, {
-	'programmaticName': 'summonMeme',
-	'name': 'Summon Potato', # is potato
-	'level': 1,
-	'manaSecondsAtLevel': 100,
-	'preferredLocation': '*',
-	'rune': Rune.new([2, 0, 1, 3, 4, 2, 1, 4, 0, 3, 2]),
-	'castFunc': Spells.dummy, # TODO
-	'botDoesNotCast': true,
+# }, {
+#	'programmaticName': 'summonBoulder',
+#	'name': 'Summon Pumpkin',
+#	'level': 3,
+#	'manaSecondsAtLevel': 25,
+#	'preferredLocation': 'ground',
+#	'rune': Rune.new([0, 1, 3, 4, 2, 0]),
+#	'castFunc': Spells.dummy, # TODO
+#	'botCooldownSeconds': 10,
+#}, {
+#	'programmaticName': 'summonFallingSpear',
+#	'name': 'Summon Carrot',
+#	'level': 3,
+#	'manaSecondsAtLevel': 10,
+#	'preferredLocation': '*',
+#	'rune': Rune.new([0, 1, 4, 0]),
+#	'castFunc': Spells.dummy, # TODO
+#	'botCooldownSeconds': 5,
+#}, {
+#	'programmaticName': 'summonExploder',
+#	'name': 'Summon Popcorn',
+#	'level': 4,
+#	'manaSecondsAtLevel': 15,
+#	'preferredLocation': 'ground',
+#	'rune': Rune.new([4, 1, 2]),
+#	'castFunc': Spells.dummy, # TODO
+#	'botCooldownSeconds': 15,
+#}, {
+#	'programmaticName': 'summonMeme',
+#	'name': 'Summon Potato', # is potato
+#	'level': 1,
+#	'manaSecondsAtLevel': 100,
+#	'preferredLocation': '*',
+#	'rune': Rune.new([2, 0, 1, 3, 4, 2, 1, 4, 0, 3, 2]),
+#	'castFunc': Spells.dummy, # TODO
+#	'botDoesNotCast': true,
 }];
 
 # Recasts the last spell. Special case since it is different per-player and changes properties.
