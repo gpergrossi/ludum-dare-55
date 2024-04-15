6143052e147f7d9a19db9472ab37f9b5707a36a2
espoire <a.pergrossi@gmail.com>
2024-04-15 19:21:06 -0400

    Reimport Egg features lost in the Unit system refactor.

--- Code/Scripts/Units/unit_egg_projectile.gd
+++ Code/Scripts/Units/unit_egg_projectile.gd
@@ -1,14 +1,39 @@
 class_name UnitEggProjectile extends UnitBase
 
+@onready var sound := %"Egg Splat Sound Player" as AudioStreamPlayer;
 @onready var egg_sprite := %EggSprite
 @onready var splat_sprite := %SplatSprite
 @onready var googley_eye := %GoogleyEyesPair
 
+enum EggMode {
+	EGG,
+	SPLAT,
+};
+
+var spin : float = randf_range(-1.5, 1.5);
+var mode : EggMode = EggMode.EGG;
+	
 func _ready() -> void:
 	state_changed.connect(_on_state_changed)
 	unit_on_ground.connect(_on_grounded)
 	super._ready()
+	scale *= randf_range(0.8, 1.2);
 
+func _process(delta):
+	if (mode == EggMode.EGG):
+		rotate_z(spin * delta);
+	
+	# Slowly shink away
+	if (mode == EggMode.SPLAT):
+		var size = scale.length();
+		var newSize = size - delta * 3;
+		if newSize < 0:
+			# Remove when fully shrunken
+			die();
+		else:
+			scale *= newSize / size;
+		
+	
 
 func _on_state_changed(_me : UnitBase, new_state : UnitState, old_state : UnitState):
 	match(new_state):
@@ -21,6 +46,13 @@ func process_unit(delta : float) -> void:
 
 
 func _on_grounded(me : UnitBase):
+	if (mode != EggMode.EGG): return;
+	
+	# Adjust position to just above the ground so the splat is better visible.
+	position.y = get_ground_height(position.x) + 0.5;
+	
+	mode = EggMode.SPLAT;
+	sound.play();
 	egg_sprite.visible = false
 	splat_sprite.visible = true
 	splat_sprite.rotation.z = randf() * TAU
@@ -30,5 +62,4 @@ func _on_grounded(me : UnitBase):
 	for target in _unit_targeting.get_targets():
 		target.take_damage(damage, knockback)
 	
-	await get_tree().create_timer(1).timeout
-	die()
+	scale *= 2;
